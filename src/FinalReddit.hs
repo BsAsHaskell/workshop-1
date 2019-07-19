c47a3ef84e9fb8130158a1a1e45b803d789e137a
clrnd <welcometothechango@gmail.com>
2019-07-19 18:14:39 -0300

    Andando con interfaz custom

--- src/FinalReddit.hs
+++ src/FinalReddit.hs
@@ -4,17 +4,15 @@ module FinalReddit
   ( run
   ) where
 
+import           Control.Monad           (forM_)
 import           Control.Monad.IO.Class
 import           Data.Aeson              (FromJSON (..), Value (..),
                                           eitherDecode, withObject, (.:))
-import           Data.Default.Class      (def)
-
 import           Data.Monoid             (mempty)
 import           Data.String.Conversions (convertString)
 import           Data.Text               (Text)
-import qualified Network.HTTP.Req        as Req
+import           Http                    (get)
 import           System.Environment      (getArgs)
-import           Text.Pretty.Simple      (pShow)
 
 data Movie = Movie
   { movieId         :: Int
@@ -24,14 +22,23 @@ data Movie = Movie
   } deriving (Show)
 
 instance FromJSON Movie where
-  parseJSON = withObject "Movie" $ \r -> Movie <$> r .: "id" <*> r .: "title" <*> r .: "overview" <*> r .: "popularity"
+  parseJSON (Object o) = do
+    id_ <- o .: "id"
+    title_ <- o .: "title"
+    overview_ <- o .: "overview"
+    popularity_ <- o .: "popularity"
+    pure (Movie id_ title_ overview_ popularity_)
+  parseJSON _ = fail "Llel"
 
-newtype Results = Results
+data Results = Results
   { results :: [Movie]
   } deriving (Show)
 
 instance FromJSON Results where
-  parseJSON = withObject "Results" $ \o -> Results <$> o .: "results"
+  parseJSON value = withObject "Results" (\o -> do
+    results_ <- o .: "results"
+    pure (Results results_)) value
+  parseJSON _ = fail "Lel"
 
 data Comando
   = Buscar String
@@ -42,18 +49,21 @@ run :: IO ()
 run = do
   args <- getArgs
   case parseArgs args of
-    Buscar query   -> llamarAPI (searchURL (convertString query))
-    Recomendar id' -> llamarAPI (recommendURL id')
+    Buscar query   -> do
+      resultados <- llamarAPI (searchURL (convertString query))
+      imprimir resultados
+    Recomendar id' -> do
+      resultados <- llamarAPI (recommendURL id')
+      imprimir resultados
 
-llamarAPI :: Text -> IO ()
-llamarAPI url =
-  Req.runReq def $ do
-    r <- Req.req Req.GET (Req.http url) Req.NoReqBody Req.jsonResponse mempty
-    liftIO $ print $ (Req.responseBody r :: Value)
+imprimir :: Results -> IO ()
+imprimir (Results resultados) = do
+  forM_ resultados imprimirPeli
+ where
+  imprimirPeli (Movie id_ titulo_ overview_ _) = putStrLn (convertString (titulo_ <> ": " <> convertString (show id_) <> "\n" <> overview_))
 
---  putStrLn (convertString (mostrar resultado))
-mostrar :: Results -> String
-mostrar = convertString . pShow
+llamarAPI :: Text -> IO Results
+llamarAPI url = Http.get url
 
 parseArgs :: [String] -> Comando
 parseArgs ["buscar", texto]  = Buscar texto
@@ -61,10 +71,10 @@ parseArgs ["recomendar", id] = Recomendar (read id)
 parseArgs _                  = error "No funciona asi papááááááa"
 
 searchURL :: Text -> Text
-searchURL query = "api.themoviedb.org/3/search/movie?api_key=" <> key <> "&query=" <> query
+searchURL query = "http://api.themoviedb.org/3/search/movie?api_key=" <> key <> "&query=" <> query
 
 recommendURL :: Int -> Text
-recommendURL id' = "api.themoviedb.org/3/movie/" <> (convertString (show id')) <> "/recommendations?api_key=" <> key
+recommendURL id' = "http://api.themoviedb.org/3/movie/" <> convertString (show id') <> "/recommendations?api_key=" <> key
 
 key :: Text
 key = "2ba61b38c35668c26d754910aac7a729"
